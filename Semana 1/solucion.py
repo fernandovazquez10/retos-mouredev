7de1f09e858abfceac449631c2a550473b0dc812
Alonso <f.vazquez151000@gmail.com>
2023-01-11 00:08:13 -0600

    :white_check_mark: Semana 1

--- Semana 1/solucion.py
+++ Semana 1/solucion.py
@@ -1,3 +1,15 @@
-# Aqui estará la solución, pero sea pronto
+leet_speak = {
+    'A': '4', 'B': 'I3', 'C': '[', 'D': ')', 'E': '3', 'F': '!=', 'G': '&',
+    'H': '#', 'I': '1', 'J': ',_|', 'K': '>|', 'L': '1', 'M': '/\/\\',  'N': '^/', 
+    'O': '0', 'P': '|*', 'Q': '(_,)', 'R': 'I2', 'S': '5', 'T': '7', 'U': '(_)', 
+    'V': '\/', 'W': '\/\/', 'X': '><', 'Y': 'j', 'Z': '2'
+}
 
-print("Hola mundo, q lo q")
+mensaje = str(input('Ingrese el mensaje que desea encriptar: '))
+mensaje_leeteado = ""
+for caracter in mensaje:
+    if leet_speak.get(caracter.upper(), None):
+        mensaje_leeteado += leet_speak[caracter.upper()]
+    else: 
+        mensaje_leeteado += caracter
+print(f"Mensaje encriptado: {mensaje_leeteado}")
