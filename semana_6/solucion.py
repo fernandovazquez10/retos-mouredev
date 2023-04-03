0f562fd8a94b8675bd135191b767c6953253f1b4
Alonso <f.vazquez151000@gmail.com>
2023-04-03 00:00:02 -0600

    Retos

---
+++ semana_6/solucion.py
@@ -0,0 +1,15 @@
+
+valores: list = ['✂️', '📄', '🗿', '🦎', '🖖']
+
+resultados =  [("🗿","✂️"), ("✂️","🗿"), ("📄","✂️")]
+
+pts_player_1 = 0
+pts_player_2 = 0
+
+for resultado in resultados:
+    player_1, __ = resultado
+    vencibles: list = []
+    if valores.index(player_1) == 5:
+        vencibles.append[valores[0]]
+    
+    
