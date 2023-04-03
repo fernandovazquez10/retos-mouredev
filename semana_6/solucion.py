34e9ecfe278fae61b9c0f4ea7e3554149d4fe129
Alonso <f.vazquez151000@gmail.com>
2023-04-03 00:12:07 -0600

    Retos

--- semana_6/solucion.py
+++ semana_6/solucion.py
@@ -7,9 +7,25 @@ pts_player_1 = 0
 pts_player_2 = 0
 
 for resultado in resultados:
-    player_1, __ = resultado
+    player_1, player_2 = resultado
     vencibles: list = []
-    if valores.index(player_1) == 5:
-        vencibles.append[valores[0]]
-    
+    index_player_1 = valores.index(player_1)
+    if index_player_1 == 5:
+        vencibles.append(valores[0])
+    else:
+        vencibles.append(valores[index_player_1 + 1])
+    if index_player_1 < 2:
+        index_player_1 += 6
+    vencibles.append(valores[index_player_1 - 2])
+    if player_2 in vencibles:
+        pts_player_1 += 1
+    else:
+        pts_player_2 += 1
+
+if pts_player_1 > pts_player_2:
+    print("Player 1")
+elif pts_player_1 < pts_player_2:
+    print("Player 2")
+else:
+    print("Tie")
     
